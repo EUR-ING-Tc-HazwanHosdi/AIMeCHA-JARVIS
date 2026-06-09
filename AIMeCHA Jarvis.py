da7d040ea4458bb521d366dda7f800f5975846b1
EUR-ING-Tc-HazwanHosdi <onezwan12@gmail.com>
2026-06-09 23:40:02 +0800

    Update AIMeCHA Jarvis.py

--- AIMeCHA Jarvis.py
+++ AIMeCHA Jarvis.py
@@ -66,7 +66,7 @@ st.markdown("""
 # ==========================================
 # HEADER WITH A.I.M.E.C.H.A. LOGO
 # ==========================================
-header_col1, header_col2 = st.columns([50, 12])
+header_col1, header_col2 = st.columns([5, 12])
 
 with header_col1:
     logo_path = "AIMeCHA Logo.png"
